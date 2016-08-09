a359e1f5e336de330f289e783e2cd4163769b0c0
Jakub Kondrat <jkondrat@soldevelo.com>
2016-08-09 19:26:14 +0200

    Revert "Fixing reference ID"
    This reverts commit 27aaf82b1adf341274c071803f1a4fe18605e221.

--- src/main/resources/initial_data.sql
+++ src/main/resources/initial_data.sql
@@ -3,4 +3,4 @@ INSERT INTO oauth_client_details (client_id, client_secret, authorities, authori
 VALUES ('trusted-client', 'secret', 'TRUSTED_CLIENT', 'client_credentials,password', 'auth,example,requisition', 'read,write');
 
 INSERT INTO auth_users (id, enabled, password, role, username, email, reference_data_user_id)
-VALUES ('51f6bdc1-4932-4bc3-9589-368646ef7ad3', 't', '$2a$10$4IZfidcJzbR5Krvj87ZJdOZvuQoD/kvPAJe549rUNoP3N3uH0Lq2G', 'ADMIN', 'admin', 'test@openlmis.org', '35316636-6264-6331-2d34-3933322d3462');
+VALUES ('51f6bdc1-4932-4bc3-9589-368646ef7ad3', 't', '$2a$10$4IZfidcJzbR5Krvj87ZJdOZvuQoD/kvPAJe549rUNoP3N3uH0Lq2G', 'ADMIN', 'admin', 'test@openlmis.org', '51f6bdc1-4932-4bc3-9589-368646ef7ad3');
