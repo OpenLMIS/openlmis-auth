1c0549c2014c50cdd27fd912f105545dd4c79774
plal <pawellal@gmail.com>
2016-08-09 16:36:15 +0200

    OLMIS-853: Changed try-catch block

--- src/main/java/org/openlmis/auth/util/Version.java
+++ src/main/java/org/openlmis/auth/util/Version.java
@@ -46,7 +46,6 @@ public class Version {
 
     InputStream inputStream = getClass().getResourceAsStream(VERSION);
     if (inputStream != null) {
-      BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
       try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
         this.service = getValueFromLine(reader.readLine());
         this.build = getValueFromLine(reader.readLine());
