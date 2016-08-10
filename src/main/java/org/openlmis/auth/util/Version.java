1ffb0f257d89116f985ecd414a69f5cf15b0fc42
plal <pawellal@gmail.com>
2016-08-10 11:36:31 +0200

    OLMIS-853: Changed way of reading version file

--- src/main/java/org/openlmis/auth/util/Version.java
+++ src/main/java/org/openlmis/auth/util/Version.java
@@ -1,40 +1,33 @@
 package org.openlmis.auth.util;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.util.Properties;
 
 /**
  * Class containing version information.
  */
 public class Version {
 
-  public static final String VERSION = "/version";
+  public static final String VERSION = "version.properties";
 
   @Getter
-  @Setter
   private String service = "service";
 
   @Getter
-  @Setter
   private String build = "${build}";
 
   @Getter
-  @Setter
   private String branch = "${branch}";
 
   @Getter
-  @Setter
   private String timeStamp = "${time}";
 
   @Getter
-  @Setter
   private String version = "version";
 
   private static final Logger LOGGER = LoggerFactory.getLogger(Version.class);
@@ -44,21 +37,18 @@ public class Version {
    */
   public Version() {
 
-    InputStream inputStream = getClass().getResourceAsStream(VERSION);
-    if (inputStream != null) {
-      try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-        this.service = getValueFromLine(reader.readLine());
-        this.build = getValueFromLine(reader.readLine());
-        this.branch = getValueFromLine(reader.readLine());
-        this.timeStamp = getValueFromLine(reader.readLine());
-        this.version = getValueFromLine(reader.readLine());
-      } catch (IOException ex) {
-        LOGGER.error("Error reading version information from version file");
+    try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(VERSION)) {
+      if (inputStream != null) {
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        service = properties.getProperty("Service");
+        build = properties.getProperty("Build");
+        branch = properties.getProperty("Branch");
+        timeStamp = properties.getProperty("Timestamp");
+        version = properties.getProperty("Version");
       }
+    } catch (IOException ex) {
+      LOGGER.error("Error loading version properties file");
     }
-  }
-
-  private String getValueFromLine(String line) {
-    return line.substring(line.indexOf(' ') + 1);
   }
 }
