7262a750a1cef68f64fb96dd8aed32a80dad470a
Jakub Hopen <jhopen@soldevelo.com>
2016-09-21 09:22:37 +0200

    OLMIS-968 Fix table names to conform to the code style guide

--- src/main/java/org/openlmis/auth/domain/User.java
+++ src/main/java/org/openlmis/auth/domain/User.java
@@ -17,7 +17,7 @@ import java.util.Collection;
 import java.util.UUID;
 
 @Entity
-@Table(name = "auth_users")
+@Table(name = "users")
 @JsonIgnoreProperties(value = { "authorities" }, ignoreUnknown = true)
 public class User extends BaseEntity implements UserDetails {
 
