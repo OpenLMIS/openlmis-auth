c8b43321717f502bf6160032c634909163fe0f5f
Chongsun Ahn <cahn922@yahoo.com>
2016-12-13 15:49:11 -0800

    OLMIS-1456 Split the client and user OAuth accounts
    Accounts attached to OpenLMIS users would use the user-client client to generate tokens, while trusted-client would simply be generated using client_credentials.

--- src/main/resources/bootstrap.sql
+++ src/main/resources/bootstrap.sql
@@ -1,4 +1,5 @@
 
-INSERT INTO auth.oauth_client_details (client_id, client_secret, authorities, authorized_grant_types,  resource_ids, scope) VALUES ('trusted-client', 'secret', 'TRUSTED_CLIENT', 'client_credentials,password', 'auth,example,requisition,notification,referencedata,fulfillment', 'read,write');
+INSERT INTO auth.oauth_client_details (client_id, client_secret, authorities, authorized_grant_types,  resource_ids, scope) VALUES ('trusted-client', 'secret', 'TRUSTED_CLIENT', 'client_credentials', 'auth,example,requisition,notification,referencedata,fulfillment', 'read,write');
+INSERT INTO auth.oauth_client_details (client_id, client_secret, authorities, authorized_grant_types,  resource_ids, scope) VALUES ('user-client', 'changeme', 'TRUSTED_CLIENT', 'password', 'auth,example,requisition,notification,referencedata,fulfillment', 'read,write');
 
 INSERT INTO auth.auth_users (id, enabled, password, role, username, email, reference_data_user_id) VALUES ('51f6bdc1-4932-4bc3-9589-368646ef7ad3', 't', '$2a$10$4IZfidcJzbR5Krvj87ZJdOZvuQoD/kvPAJe549rUNoP3N3uH0Lq2G', 'ADMIN', 'admin', 'test@openlmis.org', '35316636-6264-6331-2d34-3933322d3462');
