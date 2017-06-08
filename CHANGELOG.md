f5dca8720e6bcc8141945b7b4458b568a75efef3
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2017-06-08 18:18:19 +0200

    OLMIS-2611: Added using locale from env file.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
+3.0.2 / WIP
+===================
+
+Improvements which are backwards-compatible:
+
+* [OLMIS-2611](https://openlmis.atlassian.net/browse/OLMIS-2611): Added using locale from env file.
+
 3.0.2 / 2017-05-08
 ===================
 
--- src/main/java/org/openlmis/auth/Application.java
+++ src/main/java/org/openlmis/auth/Application.java
@@ -29,6 +29,8 @@ import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import java.util.Locale;
 
+import static org.apache.commons.lang3.LocaleUtils.toLocale;
+
 @SpringBootApplication
 @EnableAuthorizationServer
 @ImportResource("applicationContext.xml")
@@ -49,7 +51,15 @@ public class Application {
   public LocaleResolver localeResolver() {
     CookieLocaleResolver lr = new CookieLocaleResolver();
     lr.setCookieName("lang");
-    lr.setDefaultLocale(Locale.ENGLISH);
+
+    Locale systemLocale;
+    try {
+      systemLocale = toLocale(System.getenv("LOCALE"));
+    } catch (IllegalArgumentException ex) {
+      systemLocale = Locale.ENGLISH;
+    }
+    lr.setDefaultLocale(systemLocale);
+
     return lr;
   }
 
