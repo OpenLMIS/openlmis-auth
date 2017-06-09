9bb9a1e706e0bbc6c13f6021a00318e6beca7767
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2017-06-09 12:52:35 +0200

    OLMIS-2611: removed try catch statement

--- src/main/java/org/openlmis/auth/Application.java
+++ src/main/java/org/openlmis/auth/Application.java
@@ -18,6 +18,7 @@ package org.openlmis.auth;
 import org.openlmis.auth.i18n.ExposedMessageSourceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
@@ -30,6 +31,7 @@ import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import java.util.Locale;
 
 import static org.apache.commons.lang3.LocaleUtils.toLocale;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 @SpringBootApplication
 @EnableAuthorizationServer
@@ -42,6 +44,9 @@ public class Application {
     SpringApplication.run(Application.class, args);
   }
 
+  @Value("${defaultLocale}")
+  private Locale defaultLocale;
+
   /**
    * Creates new LocaleResolver.
    *
@@ -52,12 +57,9 @@ public class Application {
     CookieLocaleResolver lr = new CookieLocaleResolver();
     lr.setCookieName("lang");
 
-    Locale systemLocale;
-    try {
-      systemLocale = toLocale(System.getenv("LOCALE"));
-    } catch (IllegalArgumentException ex) {
-      systemLocale = Locale.ENGLISH;
-    }
+    String envLocale = System.getenv("LOCALE");
+    Locale systemLocale = isBlank(envLocale)
+        ? defaultLocale : toLocale(envLocale);
     lr.setDefaultLocale(systemLocale);
 
     return lr;
