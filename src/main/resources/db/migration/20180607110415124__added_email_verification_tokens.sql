bebb69ae274f9fbd3d1ff93bfb95ba1d8b90fbcc
Łukasz Lewczyński <llewczynski@soldevelo.com>
2018-06-14 12:30:55 +0200

    OLMIS-4833: Merge migrations

--- src/main/resources/db/migration/20180607110415124__added_email_verification_tokens.sql
+++ src/main/resources/db/migration/20180607110415124__added_email_verification_tokens.sql
@@ -1,10 +1,12 @@
 CREATE TABLE email_verification_tokens (
     id uuid NOT NULL,
     expirydate timestamp with time zone NOT NULL,
+    emailAddress CHARACTER VARYING(255) NOT NULL,
     userid uuid NOT NULL
 );
 
 ALTER TABLE ONLY email_verification_tokens
     ADD CONSTRAINT email_verification_tokens_pkey PRIMARY KEY (id),
     ADD CONSTRAINT email_verification_tokens_userid_uk UNIQUE (userid),
+    ADD CONSTRAINT email_verification_tokens_emailaddress_uk UNIQUE (emailAddress),
     ADD CONSTRAINT email_verification_tokens_userid_fk FOREIGN KEY (userid) REFERENCES auth_users(id);
--- src/main/resources/db/migration/20180612131415248__add_email_field_to_verification_email.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE email_verification_tokens
-ADD COLUMN emailAddress CHARACTER VARYING(255) NOT NULL;
