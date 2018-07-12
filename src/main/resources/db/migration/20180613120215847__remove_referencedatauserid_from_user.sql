d31cde1e0afd9a524bd77122180afb741f14ea6e
Łukasz Lewczyński <llewczynski@soldevelo.com>
2018-07-12 10:37:08 +0200

    OLMIS-5062: Fixed broken migration

--- src/main/resources/db/migration/20180613120215847__remove_referencedatauserid_from_user.sql
+++ src/main/resources/db/migration/20180613120215847__remove_referencedatauserid_from_user.sql
@@ -1,3 +1,15 @@
-UPDATE auth_users SET id = referencedatauserid;
+ALTER TABLE ONLY password_reset_tokens
+    DROP CONSTRAINT fk_la2ts67g4oh2sreayswhox1i6;
 
-ALTER TABLE auth_users DROP COLUMN referencedatauserid;
+UPDATE password_reset_tokens AS t
+    SET userid = u.referencedatauserid
+    FROM auth_users AS u
+    WHERE t.userid = u.id;
+
+UPDATE auth_users
+    SET id = referencedatauserid;
+
+ALTER TABLE auth_users
+    DROP COLUMN referencedatauserid;
+ALTER TABLE ONLY password_reset_tokens
+    ADD CONSTRAINT password_reset_tokens_user_id_fk FOREIGN KEY (userid) REFERENCES auth_users(id);
