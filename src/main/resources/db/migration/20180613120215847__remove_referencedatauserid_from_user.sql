1cca008d15e6b65ce35ac252e6e8bb60f4bf507a
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2019-01-10 13:26:39 +0100

    Revert "Added info about editing migrations"
    This reverts commit 451575575e445e620a66661eabe46776ab550d97.

--- src/main/resources/db/migration/20180613120215847__remove_referencedatauserid_from_user.sql
+++ src/main/resources/db/migration/20180613120215847__remove_referencedatauserid_from_user.sql
@@ -1,7 +1,3 @@
--- WHEN COMMITTING OR REVIEWING THIS FILE: Make sure that the timestamp in the file name (that serves as a version) is the latest timestamp, and that no new migration have been added in the meanwhile.
--- Adding migrations out of order may cause this migration to never execute or behave in an unexpected way. 
--- Migrations should NOT BE EDITED. Add a new migration to apply changes.
-
 ALTER TABLE ONLY password_reset_tokens
     DROP CONSTRAINT fk_la2ts67g4oh2sreayswhox1i6;
 
