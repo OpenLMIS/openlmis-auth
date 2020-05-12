5712400ede569658236947aae09ff778bfb5fda6
Chongsun Ahn <cahn922@yahoo.com>
2020-05-12 11:10:38 -0700

    Re-add API key initializer
    To restore proper access token generation on startup.

---
+++ src/main/java/org/openlmis/auth/ApiKeyInitializer.java
@@ -0,0 +1,76 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
+ */
+
+package org.openlmis.auth;
+
+import org.openlmis.auth.domain.ApiKey;
+import org.openlmis.auth.domain.ClientDetails;
+import org.openlmis.auth.repository.ApiKeyRepository;
+import org.openlmis.auth.util.Pagination;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.stereotype.Component;
+
+/**
+ * ApiKeyInitializer runs after its associated Spring application has loaded.
+ * It recreated API keys to work after the system restart.
+ */
+@Component
+@Order(20)
+public class ApiKeyInitializer implements CommandLineRunner {
+
+  @Autowired
+  private TokenStore tokenStore;
+
+  @Autowired
+  private ApiKeyRepository apiKeyRepository;
+
+  /**
+   * This method is part of CommandLineRunner and is called automatically by Spring.
+   * @param args Main method arguments.
+   */
+  public void run(String... args) {
+    Pagination.handlePage(apiKeyRepository::findAll, this::fixApiKey);
+  }
+
+  private void fixApiKey(ApiKey apiKey) {
+    OAuth2AccessToken token = createAccessToken(apiKey);
+    OAuth2Authentication authentication = createAuthentication(apiKey);
+
+    tokenStore.storeAccessToken(token, authentication);
+  }
+
+  private OAuth2AccessToken createAccessToken(ApiKey apiKey) {
+    return new DefaultOAuth2AccessToken(apiKey.getToken().toString());
+  }
+
+  private OAuth2Authentication createAuthentication(ApiKey apiKey) {
+    ClientDetails client = new ClientDetails(apiKey.getClient());
+
+    OAuth2Request storedRequest = new OAuth2Request(
+        null, client.getClientId(), client.getAuthorities(), true, client.getScope(),
+        client.getResourceIds(), null, null, null
+    );
+
+    return new OAuth2Authentication(storedRequest, null);
+  }
+
+}
